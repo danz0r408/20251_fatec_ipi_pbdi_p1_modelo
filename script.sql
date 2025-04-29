fefe02ffff24692344cf1e03f8730a7b898af809
mussoi <daniel.marques16@fatec.sp.gov.br>
2025-04-29 10:42:38 -0300

    feat(p1): encontra aprovados com pais PHD

--- script.sql
+++ script.sql
@@ -13,7 +13,22 @@ CREATE TABLE estudantes(
 -- ----------------------------------------------------------------
 -- 2 Resultado em função da formação dos pais
 --escreva a sua solução aqui
+DO $$
+DECLARE
+  cur_contagem REFCURSOR;
+  v_contagem INT;
+BEGIN
+  OPEN cur_contagem FOR
+    SELECT COUNT(*)
+    FROM estudantes
+    WHERE grade >= 5 AND (mother_edu = 6 OR father_edu = 6);
 
+  FETCH cur_contagem INTO v_contagem;
+  RAISE NOTICE 'Número de alunos aprovados com pelo menos um pai PhD: ', v_contagem;
+
+  CLOSE cur_contagem;
+END;
+$$;
 -- ----------------------------------------------------------------
 -- 3 Resultado em função dos estudos
 --escreva a sua solução aqui
