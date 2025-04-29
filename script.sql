b7e09764f86bfe86feb92d910e734b531d893da6
mussoi <daniel.marques16@fatec.sp.gov.br>
2025-04-29 10:30:53 -0300

    feat(p1): cria base e importa dados

--- script.sql
+++ script.sql
@@ -1,12 +1,18 @@
 -- ----------------------------------------------------------------
 -- 1 Base de dados e criação de tabela
 --escreva a sua solução aqui
-
-
+CREATE TABLE estudantes(
+    cod_estudantes SERIAL PRIMARY KEY,
+    studentid VARCHAR(200),
+    mother_edu INT,
+    father_edu INT,
+    grade INT,
+    salary INT,
+    prep_study INT
+);
 -- ----------------------------------------------------------------
 -- 2 Resultado em função da formação dos pais
 --escreva a sua solução aqui
-
 
 -- ----------------------------------------------------------------
 -- 3 Resultado em função dos estudos
